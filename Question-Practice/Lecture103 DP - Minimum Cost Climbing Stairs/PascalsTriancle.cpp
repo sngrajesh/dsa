059f5bf4fb25d838e78029c29867ea6e3ca9ca6a
Rajesh Singh <76207423+sngrajesh@users.noreply.github.com>
2024-09-25 23:12:24 +0530

    Create PascalsTriancle.cpp

---
+++ Question-Practice/Lecture103 DP - Minimum Cost Climbing Stairs/PascalsTriancle.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/pascals-triangle/
+/*
+                    1
+                1       1
+            1       2       1
+        1       3       3       1
+    1       4       6       4       1
+*/
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if (numRows == 0) return {};
+        if (numRows == 1) return {{1}};
+        
+        vector<vector<int>> prevRows = generate(numRows - 1);
+        vector<int> newRow(numRows, 1);
+        
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow[i] = prevRows.back()[i - 1] + prevRows.back()[i];
+        }
+        
+        prevRows.push_back(newRow);
+        return prevRows;
+    }
+};
