84c1cdb96d342b1bda166a5bf32b9222c60c98ea
Rajesh Singh <76207423+sngrajesh@users.noreply.github.com>
2025-03-01 14:01:41 +0530

    Update arrayIntersection.cpp

--- Question-Practice/Lecture010 Solving LeetCode Arrays/arrayIntersection.cpp
+++ Question-Practice/Lecture010 Solving LeetCode Arrays/arrayIntersection.cpp
@@ -1,3 +1,26 @@
+// https://leetcode.com/problems/intersection-of-two-arrays/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> hashSet(nums1.begin(), nums1.end()); 
+        unordered_set<int> result; 
+
+        for (int num : nums2) {
+            if (hashSet.count(num)) {
+                result.insert(num); 
+            }
+        }
+
+        return vector<int>(result.begin(), result.end());
+    }
+};
+
+
+
+/*
 #include <vector>
 using namespace std;
 vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
@@ -23,3 +46,4 @@ vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, i
     }
     return ans;
 }
+*/
